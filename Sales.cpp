480846fc1d720ca68af5eb284141c879fb28df2e
Michael J <30031129@cc.peralta.edu>
2024-09-08 12:33:07 -0700

    corrected some variables and added the rest of the cout prompts

--- Sales.cpp
+++ Sales.cpp
@@ -2,13 +2,14 @@
 
 int main(){
     int sales = 95000;
-    double estateTax = 0.04;
-    double countryTax = 0.02;
+    double estateTax = 0.04 * sales;
+    double countryTax = 0.02 * sales;
     double totalTax = estateTax + countryTax;
 
     std:: cout << "Total income made: " << sales << "\n";
-    std:: cout << "Estate tax owed: " << sales * estateTax;
-    std:: cout << "Country tax owed: " << sales * countryTax;
+    std:: cout << "Estate tax owed: " << estateTax;
+    std:: cout << "Country tax owed: " << countryTax;
+    std:: cout << "Total ammount of taxes owed: " << totalTax;
 
     return 0;
 }
