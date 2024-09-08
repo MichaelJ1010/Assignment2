c9078b24122e1cb46ec0454c730497dca278b0a2
Michael J <30031129@cc.peralta.edu>
2024-09-08 12:31:24 -0700

    added print messages

--- Sales.cpp
+++ Sales.cpp
@@ -5,5 +5,10 @@ int main(){
     double estateTax = 0.04;
     double countryTax = 0.02;
     double totalTax = estateTax + countryTax;
-    
+
+    std:: cout << "Total income made: " << sales << "\n";
+    std:: cout << "Estate tax owed: " << sales * estateTax;
+    std:: cout << "Country tax owed: " << sales * countryTax;
+
+    return 0;
 }
